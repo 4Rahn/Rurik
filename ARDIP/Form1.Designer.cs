99708b78d22c680d93b60bce24e36e99bd616b71
4Rahn <107988606+4Rahn@users.noreply.github.com>
2023-03-30 13:01:34 +0300

    commit to main

--- ARDIP/Form1.Designer.cs
+++ ARDIP/Form1.Designer.cs
@@ -35,20 +35,20 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button11 = new System.Windows.Forms.Button();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -64,18 +64,20 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(6, 19);
+            this.checkBox1.Location = new System.Drawing.Point(9, 29);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(199, 17);
+            this.checkBox1.Size = new System.Drawing.Size(293, 24);
             this.checkBox1.TabIndex = 0;
             this.checkBox1.Text = "Использовать избранный шаблон";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(224, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(336, 18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(960, 540);
+            this.pictureBox1.Size = new System.Drawing.Size(1440, 831);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -85,17 +87,19 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(18, 613);
+            this.textBox1.Location = new System.Drawing.Point(27, 943);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(499, 20);
+            this.textBox1.Size = new System.Drawing.Size(746, 26);
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "Путь сохранения";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(18, 587);
+            this.textBox2.Location = new System.Drawing.Point(27, 903);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(499, 20);
+            this.textBox2.Size = new System.Drawing.Size(746, 26);
             this.textBox2.TabIndex = 3;
             this.textBox2.Text = "Путь загрузки";
             // 
@@ -104,28 +108,42 @@
             this.groupBox1.Controls.Add(this.button11);
             this.groupBox1.Controls.Add(this.checkBox2);
             this.groupBox1.Controls.Add(this.checkBox1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(18, 18);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(206, 100);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(309, 154);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Робота с шаблонами";
+            this.groupBox1.Text = "Работа с шаблонами";
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(9, 100);
+            this.button11.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(162, 35);
+            this.button11.TabIndex = 13;
+            this.button11.Text = "Загрузить новый";
+            this.button11.UseVisualStyleBackColor = true;
             // 
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(6, 42);
+            this.checkBox2.Location = new System.Drawing.Point(9, 65);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(127, 17);
+            this.checkBox2.Size = new System.Drawing.Size(183, 24);
             this.checkBox2.TabIndex = 5;
             this.checkBox2.Text = "Применить ко всем";
             this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1087, 587);
+            this.button1.Location = new System.Drawing.Point(1630, 903);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(97, 46);
+            this.button1.Size = new System.Drawing.Size(146, 71);
             this.button1.TabIndex = 5;
             this.button1.Text = "Сохранить";
             this.button1.UseVisualStyleBackColor = true;
@@ -133,27 +151,30 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1109, 558);
+            this.button2.Location = new System.Drawing.Point(1664, 858);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(112, 35);
             this.button2.TabIndex = 6;
             this.button2.Text = ">>";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(1028, 558);
+            this.button3.Location = new System.Drawing.Point(1542, 858);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(112, 35);
             this.button3.TabIndex = 7;
             this.button3.Text = "<<";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(224, 558);
+            this.progressBar1.Location = new System.Drawing.Point(336, 858);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(798, 23);
+            this.progressBar1.Size = new System.Drawing.Size(1197, 35);
             this.progressBar1.TabIndex = 8;
             // 
             // groupBox2
@@ -162,105 +183,108 @@
             this.groupBox2.Controls.Add(this.button6);
             this.groupBox2.Controls.Add(this.button5);
             this.groupBox2.Controls.Add(this.button4);
-            this.groupBox2.Location = new System.Drawing.Point(13, 218);
+            this.groupBox2.Location = new System.Drawing.Point(20, 335);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(205, 210);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(308, 323);
             this.groupBox2.TabIndex = 9;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Создание нового шаблона";
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(77, 178);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(122, 23);
-            this.button4.TabIndex = 10;
-            this.button4.Text = "Сохранить шаблон";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(6, 19);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(108, 47);
-            this.button5.TabIndex = 10;
-            this.button5.Text = "Прямоугольник";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(6, 72);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(108, 47);
-            this.button6.TabIndex = 10;
-            this.button6.Text = "Многоугольник";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(5, 125);
+            this.button7.Location = new System.Drawing.Point(8, 192);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(108, 47);
+            this.button7.Size = new System.Drawing.Size(162, 72);
             this.button7.TabIndex = 10;
             this.button7.Text = "Круг";
             this.button7.UseVisualStyleBackColor = true;
             // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(9, 111);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(162, 72);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Многоугольник";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(9, 29);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(162, 72);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Прямоугольник";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(116, 274);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(183, 35);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Сохранить шаблон";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(624, 587);
+            this.button8.Location = new System.Drawing.Point(936, 903);
+            this.button8.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(99, 46);
+            this.button8.Size = new System.Drawing.Size(148, 71);
             this.button8.TabIndex = 10;
             this.button8.Text = "Обработать все";
             this.button8.UseVisualStyleBackColor = true;
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(523, 587);
+            this.button9.Location = new System.Drawing.Point(784, 903);
+            this.button9.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(95, 20);
+            this.button9.Size = new System.Drawing.Size(142, 31);
             this.button9.TabIndex = 11;
             this.button9.Text = "Изменить путь";
             this.button9.UseVisualStyleBackColor = true;
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(523, 613);
+            this.button10.Location = new System.Drawing.Point(784, 943);
+            this.button10.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(95, 20);
+            this.button10.Size = new System.Drawing.Size(142, 31);
             this.button10.TabIndex = 12;
             this.button10.Text = "Изменить путь";
             this.button10.UseVisualStyleBackColor = true;
             // 
-            // button11
-            // 
-            this.button11.Location = new System.Drawing.Point(6, 65);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(108, 23);
-            this.button11.TabIndex = 13;
-            this.button11.Text = "Загрузить новый";
-            this.button11.UseVisualStyleBackColor = true;
-            // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(18, 138);
+            this.textBox3.Location = new System.Drawing.Point(27, 212);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(52, 20);
+            this.textBox3.Size = new System.Drawing.Size(76, 26);
             this.textBox3.TabIndex = 13;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(18, 164);
+            this.textBox4.Location = new System.Drawing.Point(27, 252);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(52, 20);
+            this.textBox4.Size = new System.Drawing.Size(76, 26);
             this.textBox4.TabIndex = 14;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(86, 141);
+            this.label1.Location = new System.Drawing.Point(129, 217);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.Size = new System.Drawing.Size(119, 20);
             this.label1.TabIndex = 15;
             this.label1.Text = "Координаты X";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -268,9 +292,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(86, 167);
+            this.label2.Location = new System.Drawing.Point(129, 257);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.Size = new System.Drawing.Size(119, 20);
             this.label2.TabIndex = 16;
             this.label2.Text = "Координаты Y";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -278,9 +303,11 @@
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.checkBox3);
-            this.groupBox3.Location = new System.Drawing.Point(12, 116);
+            this.groupBox3.Location = new System.Drawing.Point(18, 178);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(206, 96);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox3.Size = new System.Drawing.Size(309, 148);
             this.groupBox3.TabIndex = 17;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Координаты";
@@ -288,18 +315,22 @@
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(6, 73);
+            this.checkBox3.Location = new System.Drawing.Point(9, 112);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(114, 17);
+            this.checkBox3.Size = new System.Drawing.Size(167, 24);
             this.checkBox3.TabIndex = 18;
             this.checkBox3.Text = "Задать курсором";
             this.checkBox3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1196, 642);
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1794, 988);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox4);
@@ -317,6 +348,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox3);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
