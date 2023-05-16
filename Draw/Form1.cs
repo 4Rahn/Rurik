d56f7344d81857868b69b8c1e406f448ea21b981
4Rahn <roman.af201@gmail.com>
2023-05-16 14:21:30 +0300

    commit to main

---
+++ Draw/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Draw
+{
+    public partial class Form1 : Form
+    {
+        private Point start;
+        private bool drawing = false;
+        public Form1()
+        {
+            InitializeComponent();
+            MouseDown += Form1_MouseDown;
+            MouseMove += Form1_MouseMove;
+            MouseUp += Form1_MouseUp;
+            Paint += Form1_Paint;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            start = e.Location;
+            drawing = true;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                Refresh();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drawing = false;
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (drawing)
+            {
+                int width = e.ClipRectangle.Width;
+                int height = e.ClipRectangle.Height;
+
+                int left = Math.Min(start.X, Cursor.Position.X) - Location.X;
+                int top = Math.Min(start.Y, Cursor.Position.Y) - Location.Y;
+                int right = Math.Max(start.X, Cursor.Position.X) - Location.X;
+                int bottom = Math.Max(start.Y, Cursor.Position.Y) - Location.Y;
+
+                Rectangle rect = new Rectangle(left, top, right - left, bottom - top);
+                e.Graphics.DrawEllipse(Pens.Black, rect);
+
+                Text = string.Format("Width: {0} Height: {1}", right - left, bottom - top);
+            }
+        }
+    }
+}
---
+++ Probnik/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Probnik
+{
+    public partial class Form1 : Form
+    {
+        public static Bitmap CombineBitmap(IEnumerable<string> files)
+        {
+            //read all images into memory
+            List<Bitmap> images = new List<Bitmap>();
+            Bitmap finalImage = null;
+
+            try
+            {
+                int width = 0;
+                int height = 0;
+
+                foreach (string image in files)
+                {
+                    // create a Bitmap from the file and add it to the list
+                    Bitmap bitmap = new Bitmap(image);
+
+                    // update the size of the final bitmap
+                    width += bitmap.Width;
+                    height = bitmap.Height > height ? bitmap.Height : height;
+
+                    images.Add(bitmap);
+                }
+
+                // create a bitmap to hold the combined image
+                finalImage = new Bitmap(width, height);
+
+                // get a graphics object from the image so we can draw on it
+                using (Graphics g = Graphics.FromImage(finalImage))
+                {
+                    // set background color
+                    g.Clear(Color.Transparent);
+
+                    // go through each image and draw it on the final image
+                    foreach (Bitmap image in images)
+                    {
+                        g.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height));
+                    }
+                }
+
+                return finalImage;
+            }
+            catch (Exception)
+            {
+                if (finalImage != null) finalImage.Dispose();
+                throw;
+            }
+            finally
+            {
+                // clean up memory
+                foreach (Bitmap image in images)
+                {
+                    image.Dispose();
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            OpenFileDialog dialog1 = new OpenFileDialog();
+            dialog1.Filter = "image Filter(*.BMP;*.JPG;*.PNG;*GIF)|*.BMP;*.JPG;*.PNG;*.GIF|All files (*.*)|*.*";
+
+            if (dialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox1.Image = new Bitmap(dialog1.FileName);
+                    var dn = Path.GetFileName(Path.GetDirectoryName(dialog1.FileName));
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно открыть выбранный файл", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = CombineBitmap(new[] { @"a:\1.png", @"a:\2.png" });
+
+            // загружаем результат в PictureBox
+            pictureBox1.Image = bitmap;
+
+            // сохраняем в файл
+            bitmap.Save(@"a:\3.png");
+        }
+    }
+}
